34a3a375f4308af6e40af2a58b33976bb5d9edd5
Carlos Sanchez <30807442+sanchezco@users.noreply.github.com>
2020-03-01 12:54:14 +0100

    Add files via upload

---
+++ 2 - Raspberry/deco_json_v1_c.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import serial
+import mysql.connector
+import json
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+#  #1:68#2:21.50@
+
+def cad_proc(cad):
+    print ("\n\nInicio------------------------------------------------>" + cad)
+    try:
+        ##y = json.dumps(cad)
+        json_decoded = json.loads(cad)
+        ##print(y)
+        #for x in json_loaded:
+    
+        print (json_decoded ['idsensor'])
+        print (json_decoded ['valor'])     
+    
+
+        sensor = json_decoded['idsensor']
+        value = json_decoded['valor']
+        #print("A")
+        print("sensor:" + str(sensor))
+        #print("B")
+        print("value:" + str(value))
+        #print("C")
+        send_mysql(str(sensor),str(value))
+    except:
+        print("ERROR FOUND")
+
+def send_mysql(sensor_,value_):
+    cnx = mysql.connector.connect(user='carlos', password='1234',
+                              host='10.100.18.122',
+                              database='iot')
+    cursor = cnx.cursor()
+    query = "Insert into data (idsensor,data) VALUES (" + sensor_  + "," + value_ + ");"
+    print(query)
+    cursor.execute(query)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+
+while True:
+    line = arduino.readline()
+    cad_proc(line)
+arduino.close() #Finalizamos la comunicacionarduino = serial.Serial('/dev/ttyACM0', 9600)
---
+++ 2 - Raspberry/deco_json_v2-no.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import serial # librería interactuar puerto serie
+import mysql.connector # libreria MySQL server
+import json # librería para decodificar json
+
+arduino = serial.Serial('/dev/ttyACM0', 9600) #puerto de conexión con arduinos
+
+def cad_proc(cad):
+    print ("\n\nInicio------------------------------------------------>" + cad)
+    try:
+        json_decoded = json.loads(cad) # Devuelve un objeto de una cadena que represente json
+
+        sensor = json_decoded['idsensor']
+        value = json_decoded['valor']
+
+        print("sensor:" + str(sensor))
+        print("value:" + str(value))
+
+        send_mysql(str(sensor),str(value)) # Manda a la base de datos el sensor id y su dato.
+        
+    except: 
+        print("ERROR FOUND")
+
+def send_mysql(sensor_,value_): # Credenciales de mi base de datos, para saber donde llevo la información.
+    cnx = mysql.connector.connect(user='carlos', password='1234',
+                              host='10.100.18.122',
+                              database='iot')
+    cursor = cnx.cursor() #interactua con MySQL server
+    print('A')
+    if ((int(sensor_) == 1) and (int(value_) < 20)): 
+        query = "Insert into data (idsensor,data,alarma) VALUES (" + sensor_  + "," + value_ + "," + "1" + ");"
+    elif ((int(sensor_) == 2) and (int(value_) > 80)):
+        query = "Insert into data (idsensor,data,alarma) VALUES (" + sensor_  + "," + value_ + "," + "1" + ");"
+    else: 
+        query = "Insert into data (idsensor,data) VALUES (" + sensor_  + "," + value_ + ");"
+
+    print(query)
+    cursor.execute(query) # Ejecuta la query
+    cnx.commit()          # Manda un commit a MySQL server, necesario después de cada query
+    cursor.close()        # Desconecto el cursor
+    cnx.close()           # Desconecto de mi base de datos
+
+while True:
+    line = arduino.readline() # lee el puerto serie de arduino
+    cad_proc(line)            # Ejecuto la función cad_proc con la informacion del puerto serie
+
+arduino.close() #Finalizamos la comunicacionarduino = serial.Serial('/dev/ttyACM0', 9600)
+
+
