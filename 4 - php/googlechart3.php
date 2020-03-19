932c672c8134097e63c692b99d2d19fa3689faa3
Carlos Sanchez <sanchezco@github.com>
2020-03-19 23:18:23 +0100

    cambio de nombre

--- 4 - php/googlechart3.php
+++ 4 - php/googlechart3.php
@@ -1,46 +1,48 @@
+// codigo completo con las graficas de temperatura, distancia y las alarmas
+
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <div id="chart_div"></div>
   <div id="chart_div2"></div>
-  <div id="chart_div3"></div>	
-  <div id="chart_div4"></div>	
-  <div id="chart_div5"></div>	
+  <div id="chart_div3"></div>
+  <div id="chart_div4"></div>
+  <div id="chart_div5"></div>
   <div id="chart_div6"></div>
-  
-  <?php 
+
+  <?php
 	//incluimos los archivos necesarios
 	include ('connectBD.php');
 	include ('funciones.php');
     // Conexion con la base de datos
     $conexion = conectarBD();
-    
+
     $valor_distancia = obtener_valores(1, $conexion);        // distancia
 	$valor_alarmadist = obtener_alarma(1, $conexion);        // alarma distancia
 	$valor_alarma_distancia = obtener_doble(1, $conexion);   // alarma y distancia
-	
+
 	$valor_distancia = obtener_valores(2, $conexion);        // temperatura
 	$valor_alarmatemp = obtener_alarma(2, $conexion);        // alarma temperatura
 	$valor_alarma_temperatura = obtener_doble(2, $conexion);   // alarma y distancia
-	
+
 	desconectarBD($conexion);
-    
+
     header("Refresh: 20; URL='a.php'"); // Actualizamos la pagina cada 20seg
 
   ?>
 
-<script> 
+<script>
     google.charts.load('current', {packages: ['corechart', 'line']});
 	google.charts.setOnLoadCallback(drawBasic1);
     google.charts.load('current', {packages: ['corechart', 'line']});
-    google.charts.setOnLoadCallback(drawBasic2);	
+    google.charts.setOnLoadCallback(drawBasic2);
     google.charts.load('current', {packages: ['corechart', 'line']});
-    google.charts.setOnLoadCallback(drawBasic3);	
+    google.charts.setOnLoadCallback(drawBasic3);
     google.charts.load('current', {packages: ['corechart', 'line']});
-    google.charts.setOnLoadCallback(drawBasic4);	
+    google.charts.setOnLoadCallback(drawBasic4);
     google.charts.load('current', {packages: ['corechart', 'line']});
-    google.charts.setOnLoadCallback(drawBasic5);	
+    google.charts.setOnLoadCallback(drawBasic5);
     google.charts.load('current', {packages: ['corechart', 'line']});
-    google.charts.setOnLoadCallback(drawBasic6);	
-		
+    google.charts.setOnLoadCallback(drawBasic6);
+
 function drawBasic1() { // represento la distancia
 
       var data = new google.visualization.DataTable();
@@ -53,7 +55,7 @@ function drawBasic1() { // represento la distancia
         title: 'SMC: Sensor de distancia',
         curveType: 'function',
         legend: { position: 'bottom' },
-		
+
 		//hAxis: {title: 'Tiempo',
 
         vAxis: {
@@ -66,9 +68,9 @@ function drawBasic1() { // represento la distancia
       };
 
       var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-      chart.draw(data, options);         
+      chart.draw(data, options);
     }
-	
+
 function drawBasic2() { // represento la alarma de distancia
 
       var data = new google.visualization.DataTable();
@@ -80,8 +82,8 @@ function drawBasic2() { // represento la alarma de distancia
      var options = {
         title: 'SMC: Alarma distancia',
         curveType: 'function',
-        legend: { position: 'bottom' },   
-		
+        legend: { position: 'bottom' },
+
         vAxis: {
           title: 'Alarma',
 			minValue: 0,
@@ -93,9 +95,9 @@ function drawBasic2() { // represento la alarma de distancia
       };
 
       var chart = new google.visualization.LineChart(document.getElementById('chart_div2'));
-      chart.draw(data, options);      
-    } 	
-	
+      chart.draw(data, options);
+    }
+
 function drawBasic3() { // represento la alarma y la distancia juntas
 
       var data = new google.visualization.DataTable();
@@ -107,7 +109,7 @@ function drawBasic3() { // represento la alarma y la distancia juntas
     var options = {
           title: 'SMC: Alarma & Distancia',
           hAxis: {title: 'Tiempo',
-		  
+
 		  titleTextStyle: {color: '#333'}},
           vAxis: {
 			minValue: 0,
@@ -118,9 +120,9 @@ function drawBasic3() { // represento la alarma y la distancia juntas
 			}
         };
 	  var chart = new google.visualization.AreaChart(document.getElementById('chart_div3'));
-      chart.draw(data, options);      
+      chart.draw(data, options);
     }
-	
+
 function drawBasic4() { // represento la temperatura
 
       var data = new google.visualization.DataTable();
@@ -132,7 +134,7 @@ function drawBasic4() { // represento la temperatura
      var options = {
         title: 'SMC: Sensor de temperatura',
         curveType: 'function',
-        legend: { position: 'bottom' }, 
+        legend: { position: 'bottom' },
 
         vAxis: {
           title: 'Temperatura (ºC)',
@@ -144,7 +146,7 @@ function drawBasic4() { // represento la temperatura
       };
 
       var chart = new google.visualization.LineChart(document.getElementById('chart_div4'));
-      chart.draw(data, options);         
+      chart.draw(data, options);
     }
 
 function drawBasic5() { // represento la alarma de temperatura
@@ -158,7 +160,7 @@ function drawBasic5() { // represento la alarma de temperatura
      var options = {
         title: 'SMC: Alarma temperatura',
         curveType: 'function',
-        legend: { position: 'bottom' }, 
+        legend: { position: 'bottom' },
         vAxis: {
           title: 'Alarma',
 			minValue: 0,
@@ -170,7 +172,7 @@ function drawBasic5() { // represento la alarma de temperatura
       };
 
       var chart = new google.visualization.LineChart(document.getElementById('chart_div5'));
-      chart.draw(data, options);      
+      chart.draw(data, options);
     }
 
 function drawBasic6() { // represento la alarma y la temperatura juntas
@@ -184,7 +186,7 @@ function drawBasic6() { // represento la alarma y la temperatura juntas
     var options = {
           title: 'SMC: Alarma & Temperatura',
           hAxis: {title: 'Tiempo',
-		  
+
 		  titleTextStyle: {color: '#000'}},
           vAxis: {
 			minValue: 0,
@@ -195,8 +197,6 @@ function drawBasic6() { // represento la alarma y la temperatura juntas
 			}
         };
 	  var chart = new google.visualization.AreaChart(document.getElementById('chart_div6'));
-      chart.draw(data, options);      
-    }	
+      chart.draw(data, options);
+    }
  </script>
-
-    
